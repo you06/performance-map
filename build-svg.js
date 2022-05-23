47a41febb79d81c91ee7dd925f5bcf3c6470bd06
you06 <you1474600@gmail.com>
2022-05-23 20:22:04 +0800

    initial commit
    Signed-off-by: you06 <you1474600@gmail.com>

---
+++ build-svg.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const child_process = require('child_process')
+
+const dir = './build'
+
+main()
+
+function main() {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, 0744);
+    }
+    const drawios = fs.readdirSync('./src')
+    drawios
+        .filter(name => name.endsWith('.drawio'))
+        .map(name => {
+            const endOffset = 7
+            return name.slice(0, name.length - endOffset)
+        })
+        .map(name => {
+            child_process.execSync(`draw.io -xf svg -o ./build/${name}.svg ./src/${name}.drawio`)
+        })
+}
